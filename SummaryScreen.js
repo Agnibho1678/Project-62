7dc48eb62b99d03599501cae6aea787a4449d4d1
Agnibho1678 <73741624+Agnibho1678@users.noreply.github.com>
2021-03-27 20:01:27 +0530

    Update SummaryScreen.js

--- SummaryScreen.js
+++ SummaryScreen.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { Text, View, StyleSheet} from 'react-native';
-import db from '../config';
+import db from 'config';
 
 class SummaryScreen extends React.Component {
   constructor() {
